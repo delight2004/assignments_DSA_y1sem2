f66c2e5884148dc5c0ab8fe32a9ca0fab5bfa380
delight2004 <delightaheebwa@gmail.com>
2025-03-02 22:12:12 +0300

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -12,3 +12,32 @@ def physics():
     average=s/4
     print(average)
 physics()
+def chemistry():
+    a=input("Assignment marks:")
+    a_num=int(a)
+    c=input("Coursework marks:")
+    c_num=int(c)
+    m=input("Mid term marks:")
+    m_num=int(m)
+    e=input("End of term marks:")
+    e_num=int(e)
+
+    s=a_num+c_num+m_num+e_num
+    average=s/4
+    print("Average for Chemistry:",average)
+chemistry()
+
+def math():
+    a=input("Assignment marks:")
+    a_num=int(a)
+    c=input("Coursework marks:")
+    c_num=int(c)
+    m=input("Mid term marks:")
+    m_num=int(m)
+    e=input("End of term marks:")
+    e_num=int(e)
+
+    s=a_num+c_num+m_num+e_num
+    average=s/4
+    print("Average for Math:",average)
+math()
